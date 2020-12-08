9705625f265aa0d62d30a4b20e9babfa3f3a18db
Gabester0 <gceipper@gmail.com>
2020-12-07 20:10:13 -0500

    Implemented first interface and type-annotation

--- index.ts
+++ index.ts
@@ -2,15 +2,25 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-axios.get(url).then( response =>{
-    const todo = response.data
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
-    const ID = todo.id
+axios.get(url).then( response =>{
+    const todo = response.data as Todo;
+
+    const id = todo.id
     const title = todo.title
-    const finished = todo.completed
-    console.log(`
-        The Todo with ID: ${ID}
-        Has a title of: ${title}
-        Is it finished? ${finished}
-    `)
+    const completed = todo.completed
+    logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) =>{
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `)
+}
